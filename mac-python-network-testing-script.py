05264d5cdadd3212f05ba3c05f1fc0bd845a3ca5
James Francies <jfrancies1@live.maryville.edu>
2021-11-30 20:06:47 -0600

    modified the mac-python-network-testing-script

--- mac-python-network-testing-script.py
+++ mac-python-network-testing-script.py
@@ -1,9 +1,15 @@
 import os, subprocess
 
+def mac_if_config_cmd():
+    print("\n--ifconfig scan that shows all of the OSs information\n")
+    os.system("ifconfig -a")
+
 def mac_os_traceroute(address):
-        os.system("traceroute {} ".format(address))
+    print("\n--Trace Route Scan \n")
+    os.system("traceroute {} ".format(address))
     
 def ping_address_function(address,how_many_scans):
+    print("\n--Ping Scan \n")
     res = subprocess.call(['ping','-c', "{}".format(how_many_scans) ,address])
     if res == 0:
         print("ping to {} OK".format(address))
@@ -17,8 +23,10 @@ def main():
     address = str(input("Enter a FQDN to scan (either www. or the ipv4 address): "))
     how_many_scans = int(input("Please enter the number of times that you would like to perform the scan: "))
 
+    
     ping_address_function(address,how_many_scans)
     mac_os_traceroute(address)
+    mac_if_config_cmd()
 
 main()
 
