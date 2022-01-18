632df185e0cb5f2b5bcde1b5338b3b5c3c172e49
James Francies <jfrancies1@live.maryville.edu>
2022-01-18 09:52:50 -0600

    updated the number of hosts file with the built-in integer function and the fact that this program is meant for wireless topology calculation

--- number_of_hosts.py
+++ number_of_hosts.py
@@ -1,8 +1,8 @@
-#Calculates the total amount of network connections for a network topology
+#Calculates the total amount of network connections for a full mesh network topology
 def main():
     number_of_hosts = int(input("Enter the number of hosts: "))
-    total_amount_of_connections = (number_of_hosts * (number_of_hosts - 1)/2)
-    print("Total connections {0}".format(total_amount_of_connections))
+    total_amount_of_connections = int((number_of_hosts * (number_of_hosts - 1)/2))
+    print("Total connections for a wireless topology {0}".format(total_amount_of_connections))
 
 
 main()
